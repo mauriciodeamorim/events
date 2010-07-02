ee1c12130ba16963490e53358212a98b87fd63db
SaaS <saas-dev@locaweb.com.br>
2010-07-02 14:42:55 -0300

    Install and setting up mongoid driver

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,9 @@
-class Event < ActiveRecord::Base
+class Event 
+  include Mongoid::Document
+    field :name
+    field :localization
+    field :url
+    field :date, :type => Date
+    field :description
+    field :available, :type => Boolean
 end
