b23ab5af15d2351aee96d1bba452c78aa6be44fc
Mauricio de Amorim <amorim@timedev.com>
2010-07-07 18:20:01 -0300

    Including spec and feature tests to about page

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe PagesController do
+  describe "handling SHOW" do
+    def do_get
+      get :show, :id => "about"
+    end
+    
+    it "should render the about page" do
+      do_get
+      response.should render_template('/pages/show/about')
+    end
+  end
+end
